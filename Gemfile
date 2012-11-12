6eadf4edb04f2e0c3047e52ead96090b0d276015
Han Wang <han.wang109@gmail.com>
2012-11-12 13:07:05 -0500

    Make basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -2,10 +2,12 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
 gem 'bootstrap-sass', '2.0.4'
+gem 'bcrypt-ruby', '3.0.1'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 group :development, :test do
+	gem 'annotate', '2.5.0'
 	gem 'sqlite3', '1.3.5'
 	gem 'rspec-rails', '2.11.0'
 	gem 'guard-rspec', '1.2.1'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,53 +2,63 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  # 仔细看一下Home Page和其他的Page的这个写法的不一样地方
-  # 简洁上来说 这种方法相当简洁
+  # Checking static page content
+
   subject {page}
+
+  shared_examples_for "all static pages" do
+    it{ should have_selector('h1',  text:heading)}
+    it{ should have_selector('title', text:full_title(page_title))}
+  end
+
 
   describe "Home page" do
     before {visit root_path}
+    let(:heading)      {'Sample App'}
+    let(:page_title)   {''}
 
-    it {should have_selector('h1',    text:'Sample App')}
-    it {should have_selector('title', text:full_title(''))}
+    it_should_behave_like "all static pages"
+
   end
 
   describe "Help page" do
-  	it "should have the content 'Help'" do
-  		visit help_path
-  		page.should have_selector('h1', :text => 'Help')
-  	end
+  	before {visit help_path}
+    let(:heading)      {'Help'}
+    let(:page_title)   {'Help'}
 
-    it "should have the right title" do
-      visit help_path
-      page.should have_selector('title',
-                :text => "Ruby on Rails Tutorial Sample App | Help")
-    end
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About Us')
-    end
+    before {visit about_path}
+    let(:heading)      {'About'}
+    let(:page_title)   {'About'}
 
-    it "should have the right title" do
-      visit about_path
-      page.should have_selector('title',
-                :text => "Ruby on Rails Tutorial Sample App | About Us")
-    end
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
-    it "should have the content 'Contact" do
-      visit contact_path
-      page.should have_selector('h1', :text => 'Contact')
-    end
+    before {visit contact_path}
+    let(:heading)      {'Contact'}
+    let(:page_title)   {'Contact'}
 
-    it "shoudld have the right title" do
-      visit contact_path  
-      page.should have_selector('title', 
-                :text => "Ruby on Rails Tutorial Sample App | Contact")
-    end
+    it_should_behave_like "all static pages"
   end
+
+  # Checking Layout
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up Now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
+  end
+
 end
